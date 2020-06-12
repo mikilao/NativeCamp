fad6fc6205a105eb8f4f4f47b93b7f6e46550866
Rachel <thelivedish@gmail.com>
2020-06-11 21:00:07 -0700

    Persist

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Main from './Components/MainComponent';
-import {Provider} from 'react-redux';
-import {ConfigureStore} from './Redux/configStore';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './Redux/configStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './Components/LoadingComponent';
 
-const store = ConfigureStore()
+const { persistor, store } = ConfigureStore()
 
 export default function App() {
   return (
     <Provider store={store}>
-    <Main />
-  </Provider>
+      <PersistGate //pt 5 prevents the app from rendering in data is rehydrated
+        loading={<Loading />}
+        persistor={persistor}>
+        <Main />
+      </PersistGate>
+    </Provider>
   );
 }
 
--- Redux/configStore.js
+++ Redux/configStore.js
@@ -4,12 +4,20 @@ import logger from 'redux-logger';
 import { campsites } from './campsites';
 import { comments } from './comments';
 import { promotions } from './promotions';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';//for persist
 import { partners } from './partners';
 import { favorites } from './favorites';
 
+const config = {//pt 1 of persist
+    key: 'root',//req
+    storage,//req
+    debug: true
+}
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({ //creates 1 single root reducer
+        persistCombineReducers(config,{//pt 2 of persist
+       // combineReducers({ //creates 1 single root reducer
             campsites,
             comments,
             partners,
@@ -18,6 +26,6 @@ export const ConfigureStore = () => {
         }),
         applyMiddleware(thunk, logger)
     );
-
-    return store;
+const persistor = persistStore(store);// pt 3 to enable the sore to be persisted
+    return {persistor, store};
 }
